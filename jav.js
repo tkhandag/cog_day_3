3f5e797b4448ffa8c270846d3430f17ee0ff9d7c
Khandagare <841698@cognizant.com>
2019-12-27 17:52:49 +0530

    d

---
+++ jav.js
@@ -0,0 +1,9 @@
+<script>
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+</script>
